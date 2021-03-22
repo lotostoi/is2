f1f8dc95a82afb367ddfcc7d101bc1638fbee2a7
Alexander Plotnikov <lotos_toi@mail.ru>
2021-03-22 19:38:16 +1000

    added  icon and bootstrap

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-<<<<<<< HEAD
-import Vuelidate from "vuelidate";
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
 
-=======
-//import { VuelidatePlugin } from "@vuelidate/core";
->>>>>>> 72ae8775d0b2d54cc1631d7e9f4ae07b306755af
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import 'font-awesome/css/font-awesome.min.css'
+
+
 
 
 createApp(App)
   .use(store)
   .use(router)
-  //.use(VuelidatePlugin)
-  .mount("#app");
+  .mount("#app")
